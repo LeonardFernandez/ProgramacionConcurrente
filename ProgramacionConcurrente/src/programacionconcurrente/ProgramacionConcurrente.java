01dc003ef8b18f36cf8fb95c16a9098b39d9f239
leonard.fernandez <leonard.fernandez@localhost>
2022-08-17 19:50:54 -0300

    Ejercicio 6

--- ProgramacionConcurrente/src/programacionconcurrente/ProgramacionConcurrente.java
+++ ProgramacionConcurrente/src/programacionconcurrente/ProgramacionConcurrente.java
@@ -12,10 +12,42 @@ package programacionconcurrente;
 public class ProgramacionConcurrente {
 
     /**
-     * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static double acceso_por_indice(double[] v, int j) {
+        try {
+            return v[j];
+        } catch (RuntimeException exc) {
+            System.out.println("El indice " + j + " no existe");
+        }
     }
+// Desde el siguiente cliente “main”:
+
+    public static void main(String[] args) {
+        double[] v = new double[15];
+        acceso_por_indice(v, 16);
+    }
+
+    /*
+    Lo que esta mal es el if, y el catch que solo lanza el error
+    public static double acceso_por_indice(double[] v, int j)
+            throws RuntimeException {
+
+        try {
+            if ((0 <= j) && (j <= v.length)) {
+                return v[j];
+            } else {
+                throw new RuntimeException("El indice " + j + " no existe en el vector");
+            }
+        } catch (RuntimeException exc) {
+            throw exc;
+        }
+    }
+// Desde el siguiente cliente “main”:
+
+    public static void main(String[] args) {
+        double[] v = new double[15];
+        acceso_por_indice(v, 16);
+    }
+     */
     
 }
